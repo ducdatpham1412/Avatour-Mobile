3a405c322071a7548917072a06b44152f6caf169
datphamamela <phamducdat14122000@gmail.com>
2023-02-17 07:34:14 +0700

    feat/config_swr

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,4 +1,5 @@
 import useTheme from './useTheme';
 import useInitApp from './useInitApp';
+import useApi from './useApi';
 
-export {useTheme, useInitApp};
+export {useTheme, useInitApp, useApi};
--- src/hook/useApi.ts
+++ src/hook/useApi.ts
@@ -1,3 +1,30 @@
-const useApi = () => {};
+import request from 'api/request';
+import useSWR, {SWRConfiguration} from 'swr';
+
+interface TypeParamsApi {
+  path: string;
+  params: Record<string, any>;
+  config: SWRConfiguration;
+}
+
+const useApi = <T>({path, params, config}: TypeParamsApi) => {
+  const {data, error, isLoading, mutate} = useSWR<T, Error>(
+    path,
+    url => request.get(url, params),
+    {
+      revalidateOnFocus: false,
+      shouldRetryOnError: false,
+      revalidateIfStale: false,
+      ...config,
+    },
+  );
+
+  return {
+    data,
+    error,
+    loading: isLoading,
+    mutate,
+  };
+};
 
 export default useApi;
